954bff532a7cd3fd88044e1ab155797967e9a01b
Niraj Pandey <nirajpandey034@gmail.com>
2023-06-25 01:39:48 +0530

    save content

--- components/SaveContent/SaveContent.util.ts
+++ components/SaveContent/SaveContent.util.ts
@@ -1,26 +1,30 @@
 import Content from "../../interfaces/Content";
 import { getCookie } from "typescript-cookie";
 const SaveContent = async (data: Content) => {
-  console.log(data);
-  const response = await fetch(
-    "https://dull-puce-badger-tux.cyclic.app/content/post-content",
-    {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "x-access-token": getCookie("token") || "",
-      },
-      body: JSON.stringify({
-        title: data.title,
-        description: data.description,
-        htmlsnippet: data.htmlsnippet,
-        csssnippet: data.csssnippet,
-        jssnippet: data.jssnippet,
-      }),
-    }
-  );
-  console.log(await response.json());
+  try {
+    const response = await fetch(
+      "https://dull-puce-badger-tux.cyclic.app/content/post-content",
+      {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: data.title,
+          description: data.description,
+          htmlsnippet: data.htmlsnippet,
+          csssnippet: data.csssnippet,
+          jssnippet: data.jssnippet,
+          token: getCookie("token"),
+        }),
+      }
+    );
+    console.log(await response.json());
+    window.location.reload();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default SaveContent;
