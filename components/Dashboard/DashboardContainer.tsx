c2ba6f171aa88710320dded1ecc61c20b1af4250
Niraj Pandey <nirajpandey034@gmail.com>
2023-06-24 18:37:36 +0530

    UI changes

--- components/Dashboard/DashboardContainer.tsx
+++ components/Dashboard/DashboardContainer.tsx
@@ -5,11 +5,23 @@ import SingleContentContainer from "./SingleContentContainer";
 import getContentWithId from "./dashboard.util";
 import Content from "../../interfaces/Content";
 import { Grid, Container } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+//  mobile view
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function DashboardContainer({ contentList }: any) {
   const [singleContent, setSingleContent] = useState<Content | undefined>();
 
   const [loading, setLoading] = useState<boolean>(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
   const getContent = async (id: string) => {
     setLoading(true);
@@ -29,12 +41,48 @@ function DashboardContainer({ contentList }: any) {
       <Grid
         container
         justifyContent="space-between"
-        sx={{ width: "90%", height: "100%", pt: 2, overflowY: "scroll" }}
+        sx={{
+          width: { xs: "100%", md: "90%" },
+          height: "100%",
+          pt: 2,
+          overflowY: "scroll",
+        }}
       >
-        <Grid item xs={3} className="shadow-lg bg-gray-50 rounded-lg">
-          <ContentList listData={contentList?.data} getContent={getContent} />
-        </Grid>
-        <Grid item xs={8}>
+        {isMobile && (
+          <Grid item xs={12} className="shadow-lg bg-gray-50 rounded-lg">
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Programs</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={age}
+                  label="Programs"
+                  // onChange={handleChange}
+                >
+                  {contentList?.data.map((item: any, index: number) => (
+                    <MenuItem
+                      value={item.id}
+                      key={index}
+                      onClick={() => {
+                        getContent(item.id);
+                      }}
+                    >
+                      {item.title}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </Grid>
+        )}
+        {isDesktop && (
+          <Grid item xs={3} className="shadow-lg bg-gray-50 rounded-lg">
+            <ContentList listData={contentList?.data} getContent={getContent} />
+          </Grid>
+        )}
+
+        <Grid item xs={12} md={8}>
           <SingleContentContainer content={singleContent} loading={loading} />
         </Grid>
       </Grid>
